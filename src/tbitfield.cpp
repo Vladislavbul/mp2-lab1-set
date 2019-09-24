02fccb745fe8d7122d459201c90ec1137ecbcf30
Vladislav Bulychev <vlad-hk.19.03@mail.ru>
2019-09-24 22:18:27 +0300

    Version1

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -9,22 +9,27 @@
 
 TBitField::TBitField(int len)
 {
+	*this;
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+	*this;
 }
 
 TBitField::~TBitField()
 {
+	*this;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+	return 0;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+	return 0;
 }
 
 // доступ к битам битового поля
@@ -51,6 +56,7 @@ int TBitField::GetBit(const int n) const // получить значение б
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
+	return *this;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
@@ -65,22 +71,27 @@ int TBitField::operator!=(const TBitField &bf) const // сравнение
 
 TBitField TBitField::operator|(const TBitField &bf) // операция "или"
 {
+	return *this;
 }
 
 TBitField TBitField::operator&(const TBitField &bf) // операция "и"
 {
+	return *this;
 }
 
 TBitField TBitField::operator~(void) // отрицание
 {
+	return *this;
 }
 
 // ввод/вывод
 
 istream &operator>>(istream &istr, TBitField &bf) // ввод
 {
+	return istr;
 }
 
 ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
+	return ostr;
 }
